32c8f0aec8977e4f53ec83568bd36d4d347f0b30
Leland Hyman <lelandhyman@gmail.com>
2021-06-24 10:32:32 -0500

    Add get_output API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SequenceEntry from './SequenceEntry';
 import ConditionEntry from './ConditionEntry';
-import PopSlider from './PopSlider';
+import PopSlider from './PopSlider'; 
 import Buttons from './Buttons';
 import Output from './Output';
 
--- src/Optimizer.js
+++ src/Optimizer.js
@@ -5,8 +5,6 @@ import { ShareablePopSizeState } from './PopSlider.js';
 import { ShareableProbeParams } from './Buttons.js';  
 import NodeFetch from 'node-fetch';
 
-const endPointAddress = 'http://127.0.0.1:8000'; // TODO: Use environment variable
-
 export const ShareableRunningState = () => {
     const [running, setRunning] = useState(false);
     return {
@@ -40,7 +38,7 @@ export const ShareableWarningState = () => {
 };
 
 async function runOptimizer(finalParams, popSize) {
-    var endPoint = endPointAddress.concat('/start_optimizer');
+    var endPoint = process.env.REACT_APP_ENDPOINTADDRESS.concat('/start_optimizer');
     finalParams.params.temperature = parseFloat(finalParams.params.temperature);
     finalParams.params.sodium = parseFloat(finalParams.params.sodium)/1000.0;
     finalParams.params.magnesium = parseFloat(finalParams.params.magnesium)/1000.0;
@@ -50,16 +48,48 @@ async function runOptimizer(finalParams, popSize) {
         method: 'POST',
         body: JSON.stringify({'probeParams': finalParams, 'popSize': popSize})
     }).then(response => {
-        if(response.status >= 400) {
+        console.log(response); 
+        // AWS API Gateway will generally timeout before the optimizer is finished. This is normal behavior.
+        if(response.status === 408 || response.status === 504) {
+            return {status: 'running'};
+        }
+        // Other errors when connecting to AWS
+        else if(response.status >= 400) {
             throw new Error('Bad response from server');
         }
         else {
             return response.json();
         }
     })
-};
+}
+
+async function getOutput(finalParams, popSize, attempt, maxAttempts) {
+    var endPoint = process.env.REACT_APP_ENDPOINTADDRESS.concat('/get_output');
+    finalParams.params.temperature = parseFloat(finalParams.params.temperature);
+    finalParams.params.sodium = parseFloat(finalParams.params.sodium)/1000.0;
+    finalParams.params.magnesium = parseFloat(finalParams.params.magnesium)/1000.0;
+    return NodeFetch(endPoint,
+    {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({'probeParams': finalParams, 'popSize': popSize})
+    }).then(response => {
+        console.log(response); 
+        if(response.status >= 400 && attempt > maxAttempts) {
+            throw new Error('Bad response from server');
+        }
+        else if (response.status >= 400) { 
+            return {status: 'error'};
+        }
+        else {
+            return response.json();
+        }
+    })    
+}
 
 const Optimizer = () => {
+
+    const maxReattempts = 5;
 
     const { probeParams } = useBetween(ShareableProbeParams);
     const { setRunning } = useBetween(ShareableRunningState);
@@ -67,12 +97,15 @@ const Optimizer = () => {
     const { setOutput } = useBetween(ShareableOutputState);
     const { popSize } = useBetween(ShareablePopSizeState);
     const { setWarning } = useBetween(ShareableWarningState);
+    const [attempts, setAttempts] = useState(0);
+    const [finalParams, ] = useState(JSON.parse(JSON.stringify(probeParams)));
+    const [finalPopSize, ] = useState(JSON.parse(JSON.stringify(popSize)));
 
     // Run on mount
     useEffect(() => {
         let mounted = true;
-        let params = JSON.parse(JSON.stringify(probeParams));
-        runOptimizer(params, popSize)
+        // Start Optimizer
+        runOptimizer(finalParams, finalPopSize)
         .then(output => {
             if(mounted) {
                 setOutput(output);
@@ -85,7 +118,36 @@ const Optimizer = () => {
             setFinished(false);
             setRunning(false);
         })
-        return () => mounted = false;
+        // Periodically check output
+        try {
+            var interval = setInterval(async () => {
+                var output = await getOutput(finalParams, finalPopSize, attempts, maxReattempts);
+                if(output.hasOwnProperty('status')) {
+                    if(output.status === 'error') {
+                        if(mounted) {
+                            setAttempts(attempts + 1);
+                        }     
+                    }
+                }
+                else { 
+                    if(mounted) {
+                        setOutput(output);
+                        setFinished(true);
+                        setRunning(false);
+                    }
+                }
+            }, 5000);
+        }
+        catch(err) {
+            console.log(err);
+            setWarning('An error occurred while connecting to the optimization server. Please try again.');
+            setFinished(false);
+            setRunning(false);
+        }
+        return () => {
+            mounted = false;
+            clearInterval(interval);
+        }
     // eslint-disable-next-line 
     }, []);
 
