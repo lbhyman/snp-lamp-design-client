a16175107b0328450f9cce47633512f14413cc0e
Leland Hyman <lelandhyman@gmail.com>
2021-11-30 11:13:43 -0600

    About styling

--- src/About.js
+++ src/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import BananaLeaf from './assets/PIXNIO-15368-1280x960.jpeg';
-import GibbsNoSink from './assets/Gibbs_no_sink_diagram.png';
-import GibbsSink from './assets/Gibbs_sink_diagram.png';
-import CombinatorialProbes from './assets/Probe_combination_diagram.png';
-import AssaySchematic from './assets/Assay_schematic.png';
-import COVIDResults from './assets/COVID Figure.png';
-import StrandDiagram from './assets/Strands_diagram.png';
+import BananaLeaf from './assets/Website Figs-01.png';
+import GibbsNoSink from './assets/Website Figs-02.png';
+import GibbsSink from './assets/Website Figs-03.png';
+import CombinatorialProbes from './assets/Website Figs-04.png';
+import AssaySchematic from './assets/Website Figs-05.png';
+import COVIDResults from './assets/Website Figs-06.png';
+import StrandDiagram from './assets/Website Figs-07.png';
 
 
 const About = () => {
@@ -77,29 +77,42 @@ const About = () => {
 
           <div className="About_sub">
             <div className="Text">
-              <h2>Assay and results</h2>
+              <h2>The assay</h2>
               <p>
                 The competitive SNP-LAMP assay is simple: simply add the primers, probes, and template to a LAMP reaction, incubate, and then anneal probes from 95C to 20C. 
-                SNP templates will yield increased fluorescence relative to WT templates or negative controls. We demonstrated this assay with several RNA targets, including 
-                an infectious mutation in the SARS-CoV-2 Spike protein. The mutated sequence was clearly distinguishable from the original, as well as the non-template background.
+                SNP templates will yield increased fluorescence relative to WT templates or negative controls. For a greater signal, this assay can also be performed in a 
+                two-step format where probes are added after amplification.
               </p>
             </div>
             <div className="Images">
               <img src={AssaySchematic} alt="Schematic of the competitive SNP-LAMP assay" />
-              <img src={COVIDResults} alt="Assay results with two SARS-CoV-2 Spike protein variants" />
             </div>
           </div>
 
           <div className="About_sub">
             <div className="Images">
-              <img src={StrandDiagram} alt="Schematic of the competitive SNP-LAMP assay strands" />
+              <img src={COVIDResults} alt="Assay results with two SARS-CoV-2 Spike protein variants" />
             </div>
+            <div className="Text">
+              <h2>Our results</h2>
+              <p>
+                We demonstrated this assay with several RNA targets, including an infectious mutation in the SARS-CoV-2 Spike protein. 
+                In this example, the mutated sequence was clearly distinguishable from the original, as well as the non-template background.
+              </p>
+            </div>
+
+          </div>
+
+          <div className="About_sub">
             <div className="Text">
               <h2>What the output means</h2>
               <p>
                 This tool will output four DNA sequences that comprise a competitive probe set. The ProbeF and ProbeQ strands represent the SNP-specific probe set containing the fluorophore 
                 and quencher, respectively. The Sink and Sink* strands are specific to the WT strand. All strand only require minor modification before they can be ordered.
               </p>
+            </div>
+            <div className="Images">
+              <img src={StrandDiagram} alt="Schematic of the competitive SNP-LAMP assay strands" />
             </div>
           </div>
 
